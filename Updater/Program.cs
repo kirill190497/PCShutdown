d52b8118e85ce3b9d562311f4cd9392e2a3d3915
kirill190497 <kirill1904@e1.ru>
2024-07-24 22:22:06 +0500

    Code refactoring, add self updater app

---
+++ Updater/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Diagnostics;
+using GlobExpressions;
+using System;
+
+
+
+namespace Updater
+{
+    static class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+           
+           
+            string dir;
+            if (args.Length == 0 )
+            {
+                dir = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+               dir  = args[0];
+               
+            }
+
+
+            
+            var zip_file = "PCShutdown.zip";
+            List<string> no_move = new List<string>
+            {
+                zip_file, "Updater.exe", "old"
+            };
+            
+            if (File.Exists(Path.Combine(dir, zip_file)))
+            {
+
+            
+            
+                var processes = Process.GetProcessesByName("PCShutdown");
+                foreach (var app in processes)
+                {
+                    app.Kill();
+                }
+            
+
+
+            // {major}.{minor}.{patch}
+
+           
+
+            
+
+            
+                var files = Glob.Files(dir, "*");
+                var directories = Glob.Directories(dir, "*");
+
+                if (!Path.Exists(Path.Combine(dir, "old")))
+                {
+                    Directory.CreateDirectory(Path.Combine(dir, "old"));
+                }
+
+                foreach (var item in files)
+                {
+                    if (!no_move.Contains(item))
+                        File.Move(Path.Combine(dir, item), Path.Combine(dir, "old", item),true);
+                }
+
+                foreach (var item in directories)
+                {
+                    if (!no_move.Contains(item))
+                        Directory.Move(Path.Combine(dir, item), Path.Combine(dir, "old", item));
+                }
+
+                System.IO.Compression.ZipFile.ExtractToDirectory(Path.Combine(dir, zip_file), dir);
+
+                
+
+                File.Delete(zip_file);
+                Directory.Delete(Path.Combine(dir, "old"), true);
+                _ = Process.Start(Path.Combine(dir, "PCShutdown.exe"));
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+
+            
+        }
+        
+    }
+}
